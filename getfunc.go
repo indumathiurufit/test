86cb9f5cd958de461175fa5b757181dec8338624
indumathiurufit <49333644+indumathiurufit@users.noreply.github.com>
2020-02-04 17:51:50 +0700

    Update getfunc.go

--- getfunc.go
+++ getfunc.go
@@ -1,50 +1,5 @@
 
-// 1.User
-func (ms *dbStorage) getUser(email string) (*atium.UserInfo, error) {
-	qs := fmt.Sprintf("%s %s %s %s",
-		"SELECT U.id, U.name, C.email, U.dob, U.gender, U.created_at, C.address,",
-		"C.primary_ph, C.secondary_ph from user U",
-		"LEFT JOIN contact C ON C.id = U.contact_id",
-		"WHERE email = ?")
 
-	var address string
-	var id int64
-	u := atium.UserInfo{}
-	row := ms.db.QueryRow(qs, email)
-
-	err := row.Scan(&id, &u.Name, &u.Email, &u.Dob, &u.Gender, &u.CreatedAt,
-		&address, &u.PrimaryPh, &u.SecondaryPh)
-	if err != nil {
-		return nil, fmt.Errorf("user not found: %v", err)
-	}
-	err = json.Unmarshal([]byte(address), &u.Address)
-	if err != nil {
-		fmt.Printf("unmarshalling address failed: %v", err)
-		fmt.Println(u.Address)
-	}
-	stats, err := getLatestStats(ms.db, id)
-	if err != nil {
-		return nil, fmt.Errorf("err getting stats: %v", err)
-	}
-	u.Stats = *stats
-	return &u, err
-}
-
-
-// 2.Client
-func (ms *dbStorage) getclient(email string) (*atium.clientInfo, error) {
-	qs := fmt.Sprintf("%s %s %s %s",
-	"select L.id,L.name,C.email, L.services,L.description,L.created_at,L.modified_at,C.address,",
- "C.primary_ph,C.secondary_ph from client L",
- "LEft JOIN contact C ON C.id= L.contact_id",
- "where email = ?")
-
- var address string
- var services string
- var description string
-	var id int64
-	l := atium.clientInfo{}
-	row := ms.db.QueryRow(qs, email)
 
 	err := row.Scan(&id,&l.Name,&l.email,&services,&description,&l.created_at,&l.modified_at,&address,
  &l.primary_ph,&l.secondary_ph)
@@ -108,22 +63,7 @@ func (ms *dbStorage) getservice(email string) (*atium.serviceInfo, error) {
 }
 
 //4.client_service
-func (ms *dbStorage) getclient_service(email string) (*atium.client_serviceInfo, error) {
-	qs := fmt.Sprintf("%s %s %s %s",
-	"select C.id,C.name,C.description,C.services,C.created_at,C.modified_at,",
-  "S.enabledAt,S.enabled from client_service S",
-  "LEft JOIN client C ON C.id = S.client_id",
-  "WHERE email = ?")
-
-	var description string
-  var services string
-	var id int64
-	u := atium.client_serviceInfo{}
-	row := ms.db.QueryRow(qs, email)
-
-	err := row.Scan(&id,&u.name,&description,&services,&u.created_at,&u.modified_at,
-  &u.enabledAt,&u.enabled)
-	if err != nil {
+func (
 		return nil, fmt.Errorf("client_service not found: %v", err)
 	}
 	err = json.Unmarshal([]byte(description), &u.description)
